170e78845dc8300323dacf68c4d4ae472206d10d
Daniel PC <daniel.barczynski@onet.pl>
2022-12-31 18:04:44 +0100

    Add project files.

---
+++ reactapi-backend/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace reactapi_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        // GET: api/<PersonController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ reactapi-backend/query.sql
@@ -0,0 +1,7 @@
+﻿insert into Persons values 
+	('Nathan', 25),
+	('Elizabeth', 26),
+	('Kayla', 27),
+	('Marcus', 28)
+
+select * from Persons
